d7e2b9218be7f58c8b24d8b412bb8bed5a5841ea
Josh Knox <jtknox@umich.edu>
2017-11-17 06:59:49 +0000

    initial game mechanics

--- js/game.js
+++ js/game.js
@@ -5,21 +5,114 @@ class PlayerBoard {
     constructor(game, state) {
         this.game = game
         this.state = state;
-    }
-    create() {
-        var squareBitmap = this.state.add.bitmapData(35 + 4, 35 + 4);
-        squareBitmap.ctx.fillStyle = '#000';
-        squareBitmap.ctx.fillRect(0, 0, 35 + 4, 35 + 4);
-        var i, j, block, square, x, y, data;
-        for(i = 0; i < 10; i++) {
-            for(j = 0; j < 10; j++) {
-                x = 36 + j * (35 + 6);
-                y = 150 + i * (35 + 6);
-                square = this.state.add.sprite(x, y, squareBitmap);
-                square.anchor.setTo(0.5);
-                square.alpha = 0.2;
+        this.rows = 12;
+        this.cols = 6;
+        this.puyoVariations = 5;
+        this.grid = [];
+        this.leftKey = game.input.keyboard.addKey(Phaser.Keyboard.LEFT);
+	    this.rightKey = game.input.keyboard.addKey(Phaser.Keyboard.RIGHT);
+	    this.downKey = game.input.keyboard.addKey(Phaser.Keyboard.DOWN);
+        this.horizontalLock = false;
+        this.verticalLock = false;
+        for(var i = 0; i < this.rows; i++) {
+            this.grid.push([]);
+            for(var j = 0; j < this.cols; j++) {
+                this.grid[i].push(0);
             }
         }
+    }
+    create() {
+        this.spawnNewPuyo();
+    }
+    print() {
+        var formatString = '';
+        formatString += '\n';
+
+        for(var i = 0; i < this.rows; i++) {
+            formatString += '\n';
+            for(var j = 0; j < this.cols; j++) {
+                formatString += ' ' + this.grid[i][j];
+            }
+        }
+        console.log(formatString);
+    }
+    checkGameOver() {
+        if(this.grid[0][2] != 0) {
+            console.log("game over");
+        }
+    }
+    spawnNewPuyo() {
+        console.log("spawning");
+        console.log(this.grid[0][2]);
+        this.game.time.events.remove(this.movementTimer);
+        this.checkGameOver();
+        this.puyo1 = Math.floor(Math.random() * this.puyoVariations) + 1;
+        this.puyo1x = 2;
+        this.puyo1y = 0
+        this.grid[0][2] = this.puyo1;
+        this.movementTimer = this.game.time.events.loop(Phaser.Timer.SECOND, this.movePuyo, this);
+        this.print();
+    }
+    checkIfPlaced() {
+        if(this.puyo1y == this.rows-1 || this.grid[this.puyo1y+1][this.puyo1x] != 0) {
+            this.game.time.events.remove(this.movementTimer);
+            this.spawnTimer = this.game.time.events.add(Phaser.Timer.SECOND, this.spawnNewPuyo, this);
+        }
+    }
+    movePuyo() {
+        this.grid[this.puyo1y][this.puyo1x] = 0;
+        this.puyo1y++;
+        this.grid[this.puyo1y][this.puyo1x] = this.puyo1;
+        this.print();
+        this.checkIfPlaced();
+    }
+    canMoveLeft() {
+        if(this.leftKey.isDown && this.puyo1x > 0 && this.grid[this.puyo1y][this.puyo1x-1] === 0 && !this.horizontalLock) {
+            return true;
+        }
+    }
+    canMoveRight() {
+        if(this.rightKey.isDown && this.puyo1x < 5 && this.grid[this.puyo1y][this.puyo1x+1] === 0 && !this.horizontalLock) {
+            return true;
+        }
+    }
+    update() {
+        if (this.canMoveLeft()) {
+            console.log("left");
+            this.grid[this.puyo1y][this.puyo1x] = 0;
+            this.puyo1x--;
+            this.grid[this.puyo1y][this.puyo1x] = this.puyo1;
+            this.horizontalLock = true;
+            this.timer = this.game.time.events.add(Phaser.Timer.SECOND/10, this.unlockHorizontalMovement, this);
+            this.checkIfPlaced();
+        }
+        if (this.canMoveRight()) {
+            console.log("right");
+            this.grid[this.puyo1y][this.puyo1x] = 0;
+            this.puyo1x++;
+            this.grid[this.puyo1y][this.puyo1x] = this.puyo1;
+            this.horizontalLock = true;
+            this.timer = this.game.time.events.add(Phaser.Timer.SECOND/10, this.unlockHorizontalMovement, this);
+            this.checkIfPlaced();
+        }
+        /*if (this.downKey.isDown && this.puyo1y < 11  && !this.verticalLock) {
+            console.log("down");
+            this.grid[this.puyo1y][this.puyo1x] = 0;
+            this.puyo1y++;
+            this.grid[this.puyo1y][this.puyo1x] = this.puyo1;
+            this.verticalLock = true;
+            this.timer = this.game.time.events.add(Phaser.Timer.SECOND/10, this.unlockVerticalMovement, this);
+            this.game.time.events.remove(this.movementTimer);
+            this.movementTimer = this.game.time.events.loop(Phaser.Timer.SECOND, this.movePuyo, this);
+            this.checkIfPlaced();
+        }*/
+        this.print();
+    }
+    unlockHorizontalMovement() {
+        this.horizontalLock = false;
+    }
+    unlockVerticalMovement() {
+        this.verticalLock = false;
     }
 };
 
@@ -102,3 +195,8 @@ PuyoPuyo.game.state.add('PreloadState', PuyoPuyo.PreloadState);
 PuyoPuyo.game.state.add('InGameState', PuyoPuyo.InGameState);
 PuyoPuyo.game.state.add('MainMenuState', PuyoPuyo.MainMenuState);
 PuyoPuyo.game.state.start('PreloadState');
+
+
+//When press a button, lock any input for a sec then unlock it
+//for left press, when press it, lock it for a 5th of a second then unlock it
+//for update, check for press and not locked
--- js/states/InGameState.js
+++ js/states/InGameState.js
@@ -7,7 +7,12 @@ PuyoPuyo.InGameState = {
         console.log("INGAME");
         this.game.stage.backgroundColor = "#FFFFFF";
         this.player1Board = new PlayerBoard(this.game, this);
+        this.player1Board.print();
         this.player1Board.create();
+    },
+    
+    update: function() {
+        this.player1Board.update();    
     },
     
     goToMainMenu() {
